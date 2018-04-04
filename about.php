c35c88ba338379255fe5690f9ab7104d7ecb43c0
Zafar Ali <z35ali@scs.ryerson.ca>
2018-04-04 14:25:39 -0400

    First commit

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+  include 'header.php';
+?>
+<!DOCTYPE html>
+
+<!--The list of group members in an unordered list -->
+<p>
+	Group 36 members:
+	<ul style="list-style-type:square">
+		<li>Zafar Ali - 500688075</li>
+		<li>Melanie Hum - 500715410</li>
+		<li>Andrei Korac - 500702565</li>
+	</ul>
+</p>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+<title>Online Art Store</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="styles.css">
+<meta charset="utf-8">
+</head>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a href="#" class="navbar-brand">Art Store</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About Us</a></li>
+      <li><a href="#">Blogs</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="" data-toggle="modal" data-target="#Modal"><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</a></li>
+    </ul>
+  </div>
+</nav>
+</html>
